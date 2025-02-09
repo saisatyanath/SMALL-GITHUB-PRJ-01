867650f9effe19aeae9a3a00a91f4eb21b95a311
saisatyanath <ksaisatyanath21@gmail>
2025-02-09 16:04:16 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# calculator.py - A simple calculator in Python
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Sum:", add(5, 3))
+    print("Difference:", subtract(5, 3))
